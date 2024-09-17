e6632a5a67eb85ae14fa7c99085b37cf361e3ff6
Joseph Brooksbank <joseph.brooksbank@navex.com>
2024-09-17 15:44:30 -0700

    initial commit

---
+++ setup_db.py
@@ -0,0 +1,13 @@
+﻿import sqlite3
+
+
+def setup():
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE table if not exists healthcheck (id INTEGER PRIMARY KEY autoincrement, status TEXT, timestamp TEXT)")
+    connection.commit()
+
+
+if __name__ == '__main__':
+    setup()
